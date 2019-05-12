f8a6aac358937595cd2ce3839bc373f79a6f0964
Alejo <motungo@hotmail.com>
2019-05-12 23:01:51 +0200

    handle 404

--- server.js
+++ server.js
@@ -33,6 +33,20 @@ mongoose.connect(
 
 // Use routes
 app.use('/api/locations', locationsRoute);
+// handle 404
+app.use((req, res, next) => {
+    res.status(404);
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({
+            errorMessage: 'The endpoint you requested could not be found',
+            status: 'failure'
+        });
+        return;
+    }
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
 
 
 const PORT = process.env.PORT || 4000;
